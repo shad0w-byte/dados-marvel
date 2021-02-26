8801acbae77623a140c6316ddc564e5e6a2ca272
Victor Kiss <victorkiss10@gmail.com>
2021-02-25 22:48:04 -0300

    nome do componente modificado na importação

--- src/data/CharacterData.jsx
+++ src/data/CharacterData.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useState,useEffect} from 'react'
 
-import CharacterCard from '../cards/characterCard/characterCard'
+import CharacterCard from '../cards/characterCard/CharacterCard'
 
 
 import InputArea from '../styles/input'
